9e1382489b733ce926c179bb08a7810f91a9dcab
Nikkex1 <niklas.pulliainen@gmail.com>
2025-06-17 13:36:29 +0300

    Exercise 15

--- part04-e15_last_week/src/last_week.py
+++ part04-e15_last_week/src/last_week.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import numpy as np
 
 def last_week():
-    return pd.DataFrame()
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    df["WoC"] -= 1
+    df = df[~df["LW"].isin(["New", "Re"])]
+    df["LW"] = df["LW"].astype(int)
+
+    mask1 = df["Peak Pos"] == df["Pos"]
+    mask2 = df["Pos"] < df["LW"]
+    df.loc[mask1 & mask2, "Peak Pos"] = np.nan
+    
+    df = df.sort_values("LW")
+    df.index = df["LW"]
+    df = df.reindex(range(1, 41))
+    df["Pos"] = df.index
+    df["LW"] = np.nan
+
+    return df
 
 def main():
-    df = last_week()
-    print("Shape: {}, {}".format(*df.shape))
-    print("dtypes:", df.dtypes)
-    print(df)
+    if True:
+        df = last_week()
+        print("Shape: {}, {}".format(*df.shape))
+        print("dtypes:", df.dtypes)
+        print(df)
+    else:
+        print(last_week())
 
 
 if __name__ == "__main__":
